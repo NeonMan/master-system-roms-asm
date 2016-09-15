d1e5e969ff373e2505e42eafe4aa9acc1d94bbc4
J.Luis <root@heavydeck.net>
2016-09-15 13:36:42 +0200

    Added lz4 skeleton. Might be useful.

---
+++ comp/lz4-sample.c
@@ -0,0 +1,10 @@
+#include <sms/intv-dummy.h>
+#include "lz4.h"
+
+void main(){
+    
+    lz4_dec();
+    while(1){
+        
+    }
+}
---
+++ comp/lz4.h
@@ -0,0 +1,8 @@
+
+#ifndef __LZ4_H
+#define __LZ4_H
+
+
+void lz4_dec();
+
+#endif
