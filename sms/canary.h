a179c2d204ab708ac8de6cd09bdb3ae2b1e82740
J.Luis <root@heavydeck.net>
2016-09-29 00:01:42 +0200

    Added 'canary.h' to ease detection of overflows with RAM Crt0.

---
+++ sms/canary.h
@@ -0,0 +1,33 @@
+#ifndef __CANARY_H
+#define __CANARY_H
+
+static const unsigned char canary_value[2] = {0x16, 0xe9};
+
+#ifndef CANARY_ADDRESS
+extern unsigned char* crt0_canary;
+#else
+#define crt0_canary ((unsigned char*) CANARY_ADDRESS)
+#endif
+
+#ifdef CANARY_SAFE_RETURN
+#define return canary_test(); return
+#endif
+
+static void canary_check(){
+    if(
+        (crt0_canary[0] != canary_value[0]) ||
+        (crt0_canary[1] != canary_value[1])
+    ){
+        /*Canary died. Reset the console*/
+        __asm
+        JP #0x0000
+        __endasm;
+    }
+}
+
+static void canary_set(){
+    crt0_canary[0] = canary_value[0];
+    crt0_canary[1] = canary_value[1];
+}
+
+#endif
