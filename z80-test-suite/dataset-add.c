bc38e1da722c3e1ea1a1accc788f21104d783d31
J.Luis <root@heavydeck.net>
2016-12-31 03:12:32 +0100

    Skeleton test ROM.

--- z80-test-suite/dataset-add.c
+++ z80-test-suite/dataset-add.c
@@ -1,6 +1,40 @@
+/**
+ * @file  dataset-add.c
+ * @brief exhaust the ADD alu operations.
+ *
+ * Inputs: A, B
+ * Outputs: (A + B), Flags
+ */
+#include <sms/uart.h>
+#include <stdint.h>
+
+static void print(char* str){
+    while(*str){
+        uart_putc(*str);
+        str++;
+    }
+}
+
+static const char hex_lut[17] = "0123456789ABCDEF";
+static void print_hex(uint8_t b){
+    uart_putc(hex_lut[(b>>4) & 0x0F]);
+    uart_putc(hex_lut[(b   ) & 0x0F]);
+}
+
+static void do_test(){
+    
+}
 
 void main(){
-    /**/
+    /*Send header through UART*/
+    print("/*" __FILE__ "*/");
+    print("static const uint16_t result[] = {\n");
+    
+    /*Send payload*/
+    do_test();
+    
+    /*Send tail through UART*/
+    print("\n};\n");
     
     /*Reboot*/
     {
