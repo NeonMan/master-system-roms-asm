3a3b2e27554ee2342355a58cc585f013f165345c
J.Luis <root@heavydeck.net>
2017-11-12 00:15:04 +0100

    Added XMODEM CRC table.

--- noramboot/build.bat
+++ noramboot/build.bat
@@ -3,5 +3,7 @@ cls
 sdasz80 -l -o -s -g noramboot.S
 sdasz80 -l -o -s -g vdp.S
 sdasz80 -l -o -s -g font.S
+sdasz80 -l -o -s -g crc16-xmodem.S
 sdcc -o noramboot.ihx --code-loc 0100 *.rel
 makebin noramboot.ihx noramboot.sms
+pause
