17a4d0e354557cfbad8f4a4b15888f77b88c756a
J.Luis <root@heavydeck.net>
2017-11-12 02:07:31 +0100

    Added SDSC info.

--- noramboot/build.bat
+++ noramboot/build.bat
@@ -5,5 +5,5 @@ sdasz80 -l -o -s -g vdp.S
 sdasz80 -l -o -s -g font.S
 sdasz80 -l -o -s -g crc16-xmodem.S
 sdcc -o noramboot.ihx --code-loc 0100 *.rel
-makebin noramboot.ihx noramboot.sms
+makebin -p noramboot.ihx noramboot.sms
 pause
