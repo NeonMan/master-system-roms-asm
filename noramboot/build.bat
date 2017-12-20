91d1e7166ae64c9d6056f4b9dca419b27e78643b
J.Luis <root@heavydeck.net>
2017-12-20 21:56:49 +0100

    Added test serial mock.

--- noramboot/build.bat
+++ noramboot/build.bat
@@ -1,13 +1,20 @@
 @echo off
 call clean.bat
 cls
+
 sdasz80 -l -o -s -g noramboot.S
 sdasz80 -l -o -s -g vdp.S
 sdasz80 -l -o -s -g font.S
 sdasz80 -l -o -s -g xmodem-boot.S
 sdasz80 -l -o -s -g test-cr16-xmodem.S
+sdasz80 -l -o -s -g test-serial.S
+
 sdcc -o noramboot.ihx -mz80 --no-std-crt0 --code-loc 128 xmodem-boot.rel font.rel vdp.rel noramboot.rel
 sdcc -o test-crc16-xmodem.ihx -mz80 --no-std-crt0 --code-loc 128 font.rel vdp.rel test-cr16-xmodem.rel
+sdcc -o test-serial.ihx -mz80 --no-std-crt0 --code-loc 128 font.rel vdp.rel test-serial.rel
+
 makebin -p noramboot.ihx noramboot.sms
 makebin -p test-crc16-xmodem.ihx test-crc16-xmodem.sms
+makebin -p test-serial.ihx test-serial.sms
+
 pause
