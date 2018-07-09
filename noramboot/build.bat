eddeb2845e21681e58fbb91aae94ac27682c7aa4
J.Luis <root@heavydeck.net>
2018-07-10 01:29:21 +0200

    Added Clear/Set carry macro; Tuned serial TX to 4800bps.

--- noramboot/build.bat
+++ noramboot/build.bat
@@ -10,14 +10,20 @@ sdasz80 -l -o -s -g test-cr16-xmodem.S
 sdasz80 -l -o -s -g test-serial.S
 
 rem --- 128 for normal ROM
-rem --- 49152 for RAM base
-sdcc -o noramboot.ihx -mz80 --no-std-crt0 --code-loc 128 xmodem-boot.rel font.rel vdp.rel noramboot.rel
-sdcc -o test-crc16-xmodem.ihx -mz80 --no-std-crt0 --code-loc 128 font.rel vdp.rel test-cr16-xmodem.rel
-sdcc -o test-serial.ihx -mz80 --no-std-crt0 --code-loc 128 font.rel vdp.rel test-serial.rel
+rem --- 49152 for RAM base (+128 -> 49280)
+sdcc -o noramboot.ihx -mz80 --no-std-crt0 --code-loc 128 --no-peep --no-peep-return xmodem-boot.rel font.rel vdp.rel noramboot.rel
+sdcc -o test-crc16-xmodem.ihx -mz80 --no-std-crt0 --code-loc 128 test-cr16-xmodem.rel font.rel vdp.rel
+sdcc -o test-serial.ihx -mz80 --no-std-crt0 --code-loc 128 --no-peep --no-peep-return font.rel vdp.rel test-serial.rel
 
 makebin -p noramboot.ihx noramboot.sms
 makebin -p test-crc16-xmodem.ihx test-crc16-xmodem.sms
 makebin -p test-serial.ihx test-serial.sms
+
+rem --- RAM versions
+rem sdcc -o test-crc16-xmodem-ram.ihx -mz80 --no-std-crt0 --code-loc 49152 test-cr16-xmodem.rel font.rel vdp.rel 
+rem makebin -p -s 65535 test-crc16-xmodem-ram.ihx test-crc16-xmodem-ram-raw.sms
+rem python ../scripts/trim.py test-crc16-xmodem-ram-raw.sms test-crc16-xmodem-ram.sms 49152
+
 
 python ..\scripts\fix-checksum.py noramboot.sms
 python ..\scripts\fix-checksum.py test-crc16-xmodem.sms
