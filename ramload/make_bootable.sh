549b02e84ef4da863ac47e6c4fb69b4adc519cb2
J.Luis <root@heavydeck.net>
2019-05-31 16:32:39 +0200

    make_bootable now adding VRAM padding

--- ramload/make_bootable.sh
+++ ramload/make_bootable.sh
@@ -11,6 +11,7 @@ fi
 ROM_SIZE=`stat "$1" --format=%s`
 if [ $? -ne 0 ]
 then
+  echo "Usage: $0 ROM_DATA [VRAM_DATA]"
   exit 1
 fi
 if [ $ROM_SIZE -gt 8192 ]
@@ -31,13 +32,14 @@ then
   echo "VRAM size too big: $VRAM_SIZE > 16384"
   exit 1
 fi
+PADDING_VRAM_SIZE=`expr 16384 - $VRAM_SIZE`
 
 #Show info
 echo "Loader ROM: $LOADER_ROM"
 echo "       ROM: $1"
 echo "  ROM size: $ROM_SIZE (+$PADDING_SIZE padding)"
 echo "  VRAM ROM: $2"
-echo " VRAM size: $VRAM_SIZE"
+echo " VRAM size: $VRAM_SIZE (+$PADDING_VRAM_SIZE padding)"
 
 #Append the ROM data
 OUT_FILE="$1.boot.sms"
@@ -55,4 +57,19 @@ fi
 if [ $VRAM_SIZE -gt 0 ]
 then
   cat "$2" >> "$OUT_FILE"
+fi
+
+#Add VRAM padding
+if [ $PADDING_VRAM_SIZE -gt 0 ]
+then
+  SEEK_ADDR=`expr 16384 + $VRAM_SIZE`
+  dd if=/dev/zero of="$OUT_FILE" bs=1 count=$PADDING_VRAM_SIZE seek=$SEEK_ADDR 2>/dev/null
+fi
+
+#Resulting ROM must be *exactly* 32K
+OUT_SIZE=`stat "$OUT_FILE" --format=%s`
+if [ $OUT_SIZE -ne 32768 ]
+then
+  echo "Resulting ROM size missmatch (Bug?)"
+  exit 1
 fi
